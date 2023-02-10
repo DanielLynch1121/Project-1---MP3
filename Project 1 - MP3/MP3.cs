67a783eb45c670102aacd14c8efd7f0339f231b3
Daniel Lynch <cplayma1@outlook.com>
2023-02-09 20:25:25 -0500

    d

--- Project 1 - MP3/MP3.cs
+++ Project 1 - MP3/MP3.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/**
+*--------------------------------------------------------------------
+* File name: {MP3.cs}
+* Project name: {Project 1 - MP3}
+*--------------------------------------------------------------------
+* Author’s name and email: {Daniel Lynch ynchda@etsu.edu}
+* Course-Section: {002}
+* Creation Date: {02/02/2023}
+* -------------------------------------------------------------------
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Project 1 - MP3/Program.cs
+++ Project 1 - MP3/Program.cs
@@ -1,40 +1,86 @@
-﻿namespace Project_1___MP3
+﻿/**
+*--------------------------------------------------------------------
+* File name: {Program.cs}
+* Project name: {Project 1 - MP3}
+*--------------------------------------------------------------------
+* Author’s name and email: {Daniel Lynch ynchda@etsu.edu}
+* Course-Section: {002}
+* Creation Date: {02/02/2023}
+* -------------------------------------------------------------------
+*/
+namespace Project_1___MP3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             CenterText("Project: MP3 Tracker");
-            CenterText("Project 1");
+            CenterText("Demonstrates the functionality of the MP3 class.");
             CenterText("-------------------");
             CenterText("Created By: Daniel Lynch");
-            CenterText("-----------------------------");
+            CenterText("---------------------------");
 
             string name;
-            CenterText("Please Enter Your Name:");
+            Console.Write("     Please Enter Your Name:");
             name = Console.ReadLine();
             CenterText("Press <Enter> to continue");
             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
             Console.Clear();
+            
 
             do
             {
                 int userInput;
                 CenterText("Menu");
-                CenterText("--------------------------------------");
+                CenterText("--------------------------------");
                 CenterText("1. Create a New MP3 File");
                 CenterText("2. Display an MP3 File");
                 CenterText("3. Exit");
                 Console.Write("Type The Number Of Your Choice:");
                 userInput = int.Parse(Console.ReadLine());
+                
+                string songTitle;
+                string artist;
+                string songReleaseDate;
+                double playbackTimeInMinutes;
+                string genre;
+                Genre getParse;
+                decimal downloadCost;
+                double fileSizeInMBs;
+                string albumCoverPhoto;
+
+                MP3 userMP3;
 
                 if(userInput == 1)
                 {
-                    CenterText("Create an MP3");
+                    Console.Clear();
+                    Console.WriteLine("Create an MP3");
+                    Console.WriteLine("-----------------------------");
+                    Console.Write("1. Please Enter The Songs Title:");
+                    songTitle = Console.ReadLine();
+                    Console.Write("2. Please Enter The Artit's Name:");
+                    artist = Console.ReadLine();
+                    Console.Write("3. Please Enter The Release Date (MM/DD/YYYY): ");
+                    songReleaseDate = Console.ReadLine();
+                    Console.Write("4. Please Enter The Playback Time (Minutes):");
+                    playbackTimeInMinutes = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("5. Please Enter The Song's Genre (Rock, Pop, Jazz, Country, Classical, Lofi, Funk, Rap, Other):");
+                    genre = Console.ReadLine();
+                    Enum.TryParse(genre, out getParse);
+                    Console.Write("6. Please Enter The Download Cost:");
+                    downloadCost = Convert.ToDecimal(Console.ReadLine());
+                    Console.Write("7. Please Enter The File Size (MB):");
+                    fileSizeInMBs = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("8. Please Enter the Path Of The Album Cover Photo:");
+                    albumCoverPhoto = Console.ReadLine();
+
+                    userMP3 = new MP3(songTitle, artist, songReleaseDate, playbackTimeInMinutes, getParse, downloadCost, fileSizeInMBs, albumCoverPhoto);
+                    
                 }
                 else if(userInput == 2)
                 {
-
+                    Console.Clear();
+                    
                 }
                 else if(userInput == 3)
                 {
