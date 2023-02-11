a7ef43f2a136d97d21f8af96c03ba2aa0272a6ce
Daniel Lynch <cplayma1@outlook.com>
2023-02-10 19:35:47 -0500

    fixed issue with do while loop

--- Project 1 - MP3/Program.cs
+++ Project 1 - MP3/Program.cs
@@ -27,9 +27,11 @@ namespace Project_1___MP3
             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
             Console.Clear();
             
+            MP3 userMP3 = null;
 
             do
             {
+                Console.Clear() ;
                 int userInput;
                 CenterText("Menu");
                 CenterText("--------------------------------");
@@ -38,7 +40,7 @@ namespace Project_1___MP3
                 CenterText("3. Exit");
                 Console.Write("Type The Number Of Your Choice:");
                 userInput = int.Parse(Console.ReadLine());
-                
+
                 string songTitle;
                 string artist;
                 string songReleaseDate;
@@ -49,48 +51,60 @@ namespace Project_1___MP3
                 double fileSizeInMBs;
                 string albumCoverPhoto;
 
-                MP3 userMP3;
-
-                if(userInput == 1)
+                switch(userInput)
                 {
-                    Console.Clear();
-                    Console.WriteLine("Create an MP3");
-                    Console.WriteLine("-----------------------------");
-                    Console.Write("1. Please Enter The Songs Title:");
-                    songTitle = Console.ReadLine();
-                    Console.Write("2. Please Enter The Artit's Name:");
-                    artist = Console.ReadLine();
-                    Console.Write("3. Please Enter The Release Date (MM/DD/YYYY): ");
-                    songReleaseDate = Console.ReadLine();
-                    Console.Write("4. Please Enter The Playback Time (Minutes):");
-                    playbackTimeInMinutes = Convert.ToDouble(Console.ReadLine());
-                    Console.Write("5. Please Enter The Song's Genre (Rock, Pop, Jazz, Country, Classical, Lofi, Funk, Rap, Other):");
-                    genre = Console.ReadLine();
-                    Enum.TryParse(genre, out getParse);
-                    Console.Write("6. Please Enter The Download Cost:");
-                    downloadCost = Convert.ToDecimal(Console.ReadLine());
-                    Console.Write("7. Please Enter The File Size (MB):");
-                    fileSizeInMBs = Convert.ToDouble(Console.ReadLine());
-                    Console.Write("8. Please Enter the Path Of The Album Cover Photo:");
-                    albumCoverPhoto = Console.ReadLine();
-
-                    userMP3 = new MP3(songTitle, artist, songReleaseDate, playbackTimeInMinutes, getParse, downloadCost, fileSizeInMBs, albumCoverPhoto);
+                    case 1:
                     
-                }
-                else if(userInput == 2)
-                {
-                    Console.Clear();
-                    
-                }
-                else if(userInput == 3)
-                {
-                    CenterText("Goodbye, Thank you for choosing MP3 Tracker,");
-                    CenterText(name);
-                    Environment.Exit(0);
-                }
-                else
-                {
-                    Console.WriteLine("Please Try Agian");
+                        Console.Clear();
+                        Console.WriteLine("       Create an MP3");
+                        Console.WriteLine("-----------------------------");
+                        Console.Write("1. Please Enter The Songs Title:");
+                        songTitle = Console.ReadLine();
+                        Console.Write("2. Please Enter The Artit's Name:");
+                        artist = Console.ReadLine();
+                        Console.Write("3. Please Enter The Release Date (MM/DD/YYYY): ");
+                        songReleaseDate = Console.ReadLine();
+                        Console.Write("4. Please Enter The Playback Time (Minutes):");
+                        playbackTimeInMinutes = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("5. Please Enter The Song's Genre (Rock, Pop, Jazz, Country, Classical, Lofi, Funk, Rap, Other):");
+                        genre = Console.ReadLine();
+                        Enum.TryParse(genre, out getParse);
+                        Console.Write("6. Please Enter The Download Cost:");
+                        downloadCost = Convert.ToDecimal(Console.ReadLine());
+                        Console.Write("7. Please Enter The File Size (MB):");
+                        fileSizeInMBs = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("8. Please Enter the Path Of The Album Cover Photo:");
+                        albumCoverPhoto = Console.ReadLine();
+
+                        userMP3 = new MP3(songTitle, artist, songReleaseDate, playbackTimeInMinutes, getParse, downloadCost, fileSizeInMBs, albumCoverPhoto);
+                        break;
+                    case 2:
+                
+                        Console.Clear();
+                        if (userMP3 != null)
+                        {
+                            Console.WriteLine(userMP3.ToString());
+                            CenterText("Press <Enter> to continue");
+                            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                        }
+                        else
+                        {
+                            CenterText("No MP3 object exists. Please create one first.");
+                            CenterText("Press <Enter> to continue");
+                            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                        }
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        CenterText("Goodbye, Thank you for choosing MP3 Tracker,");
+                        CenterText(name);
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Please Enter a Number 1-3");
+                        break;
                 }
             }
             while(true);
